6a7cc37260f25249e17e2b2b8f144e9132009870
Eric-christianson-1 <159150444+Eric-christianson-1@users.noreply.github.com>
2024-02-21 13:53:38 +0000

    unit 1-07 edits

--- handler.php
+++ handler.php
@@ -16,7 +16,13 @@
     }
     echo "<h1>Welcome ".$userName."!</h1>\n";
     ?>
+
+ <form action="welcome_get.php" method="get">
+ Name: <input type="text" name="name"><br>
+ E-mail: <input type="text" name="email"><br>
+ <input type="submit">
+ </form>
     
-  </body>
+ </body>
   
 </html>
