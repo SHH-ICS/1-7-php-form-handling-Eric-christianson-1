87d0ea752cafdd6a8dafe28a422e72dbe3ebc67d
Eric-christianson-1 <159150444+Eric-christianson-1@users.noreply.github.com>
2024-02-28 13:45:41 +0000

    unit 1-07 edits

--- handler.php
+++ handler.php
@@ -22,6 +22,10 @@
  E-mail: <input type="text" name="email"><br>
  <input type="submit">
  </form>
+
+ Welcome <?php echo $_POST["name"]; ?><br>
+Your email address is: <?php echo $_POST["email"]; ?>
+
     
  </body>
   
